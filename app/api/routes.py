ac086bce9900e4aaa654be427b192b8a77d653cd
Gabriel Sollero <sollero.gms@gmail.com>
2025-01-29 16:57:13 -0300

    feat: serve static PDF in /invoice

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,9 @@
+import io
 import os
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, send_file, jsonify
+from jinja2 import Environment, FileSystemLoader
+from weasyprint import HTML
 
 blueprint = Blueprint("api", __name__)
 
@@ -9,4 +12,15 @@ TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "../templates")
 
 @blueprint.route("/invoice", methods=["GET"])
 def invoice():
-    return jsonify({"message": "hello world!"})
+    try:
+        env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+        template = env.get_template("invoice/index.html")
+        html_str = template.render()
+
+        pdf_buffer = io.BytesIO()  # alternative to temp file
+        HTML(string=html_str).write_pdf(pdf_buffer, stylesheets=[os.path.join(TEMPLATE_DIR, "invoice", "style.css")])
+        pdf_buffer.seek(0)
+
+        return send_file(pdf_buffer, mimetype="application/pdf", as_attachment=True, download_name="document.pdf")
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
