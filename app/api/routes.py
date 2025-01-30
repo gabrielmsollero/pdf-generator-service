c1e9cbfaa462a3e5c87fe149e0217ef4e4306b4f
Gabriel Sollero <sollero.gms@gmail.com>
2025-01-30 16:32:25 -0300

    chore: add logging to route handler

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,10 +1,12 @@
+import json
 import io
 import os
 
-from flask import Blueprint, jsonify, request, send_file
+from flask import current_app, Blueprint, jsonify, request, send_file
 from jinja2 import Environment, FileSystemLoader
 from marshmallow import Schema, fields, ValidationError
 from weasyprint import HTML
+from werkzeug.exceptions import HTTPException
 
 blueprint = Blueprint("api", __name__)
 
@@ -31,20 +33,32 @@ invoice_schema = InvoiceSchema()
 
 @blueprint.route("/invoice", methods=["POST"])
 def invoice():
+    logger = current_app.logger
     try:
+        logger.info(f"receiving request for template 'invoice'")
+        logger.info(json.dumps(request.json, indent=2))
         try:
             data = invoice_schema.load(request.json)
         except ValidationError as err:
+            logger.info("validation error while processing request body")
+            logger.info(json.dumps(err.messages, indent=2))
             return jsonify({"error": err.messages}), 400
 
         env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
         template = env.get_template("invoice/index.html")
+        logger.info("rendering document")
         html_str = template.render(**data)
 
+        logger.info("writing to PDF")
         pdf_buffer = io.BytesIO()  # alternative to temp file
         HTML(string=html_str).write_pdf(pdf_buffer, stylesheets=[os.path.join(TEMPLATE_DIR, "invoice", "style.css")])
         pdf_buffer.seek(0)
 
+        logger.info("sending response")
         return send_file(pdf_buffer, mimetype="application/pdf", as_attachment=True, download_name="document.pdf")
+    except HTTPException as e:
+        logger.info(f"http exception while processing request: {e.description}")
+        return jsonify({"error": e.description}), e.code
     except Exception as e:
+        logger.exception(f"unexpected error while processing request:")
         return jsonify({"error": str(e)}), 500
